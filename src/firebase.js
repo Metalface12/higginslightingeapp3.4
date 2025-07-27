bb180646a204a530170adcfa2bcfa032b3b34c16
Metalface12 <joeyhiggins@hotmail.com>
2025-07-26 21:43:44 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
 // src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey:      process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain:  process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId:   process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId:       process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey:               process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:           process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:            process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:        process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:                process.env.REACT_APP_FIREBASE_APP_ID
 };
 
+// Initialize Firebase & export the Firestore instance
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
